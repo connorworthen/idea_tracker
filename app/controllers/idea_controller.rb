d2856188bbece37dab79c9d8ecf938ebb4fdd172
connorworthen <56981138+connorworthen@users.noreply.github.com>
2020-05-06 12:20:17 -0700

    Add files via upload

---
+++ app/controllers/idea_controller.rb
@@ -0,0 +1,28 @@
+require 'pry'
+class IdeaController < ApplicationController
+
+  get '/ideas/:id/edit' do
+    redirect_login
+    @idea = Idea.find(params[:id])
+    current_user_owns_idea
+    erb :'/ideas/edit'
+  end
+
+  patch '/ideas/:id/edit' do
+    redirect_login
+    @idea = Idea.find(params[:id])
+    current_user_owns_idea
+    @idea.update(params[:idea])
+    redirect to "/lists/#{List.find((@idea.list_id)).slug}"
+  end
+
+
+  helpers do
+    def current_user_owns_idea
+      if current_user.id != List.find_by(id: @idea.list_id).user_id
+        redirect to '/'
+      end
+    end
+  end
+  
+end

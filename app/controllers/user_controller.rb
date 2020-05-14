270eea44e6ff2f44a971535b88afc7b91b5393c9
connorworthen <connorw220@protonmail.com>
2020-05-13 20:01:50 -0700

    DRY

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,10 +6,6 @@ class UserController < ApplicationController
     erb :"/users/show.html"
   end
 
-  get "/login" do
-    erb :"/users/login"
-  end
-
   get "/signup" do
     erb :"/users/signup"
   end
@@ -28,11 +24,21 @@ class UserController < ApplicationController
     end
   end
 
-  post "/login" do
-    @user = User.find_by(:username => params[:username])
-    if @user && @user.authenticate(params[:password])
-      session[:user_id] = @user.id
-      redirect '/users' # add rack flash error msg/ error file
+  get "/login" do
+    erb :"/users/login"
+  end
+
+  post '/login' do
+    user = User.find_by(username: params[:username])
+    if !user
+      flash[:message] = "There is no account associated with the username: #{params[:username]}"
+      redirect to '/login'
+    elsif user && user.authenticate(params[:password])
+      session[:id] = user.id
+      redirect to '/users'
+    else
+      flash[:message] = "The username or password combination is incorrect"
+      redirect to '/login'
     end
   end
 
